0e89e42d63480f67d96f4223b43be340eb2a46ac
William-Boomer-Baker <61856702+William-Boomer-Baker@users.noreply.github.com>
2021-12-02 12:31:42 -0800

    Create appointment.js

---
+++ pages/appointment.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Appointment() {
+  return (
+      <div className="section-54 wf-section">
+          <div className="div-block-137">
+            <h1 className="heading-55">Get In Touch With Us!</h1>
+            <p className="paragraph-37">Want to go ahead and schedule an appointment? Use the calendar below to find the best time that works for you.</p>
+          </div>
+          <div className="html-embed w-embed">
+            {/*  Calendly inline widget begin  */}
+            <InlineWidget
+              url="https://calendly.com/hyperscalemedia/30min"
+              styles={{
+                minWidth: '320px',
+                height: '675px'
+              }}
+            />
+            {/*  Calendly inline widget end  */}
+          </div>
+        </div>
+  )
+}
