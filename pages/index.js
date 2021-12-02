2474074d907d3a0f9a224ddafaa8468c54c76bc3
William-Boomer-Baker <61856702+William-Boomer-Baker@users.noreply.github.com>
2021-12-02 12:28:10 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
               <h1 className="heading-63" />
               <p className="paragraph-44">We help you effortlessly scale your design team with access to a team composed of 100% UI/UX designers with Fortune 500 or venture-backed startup experience.</p>
               <div className="form-block w-form">
-                <form id="email-form" name="email-form" data-name="Email Form" method="post" action="/.netlify/functions/next_api_submit" className="form-4"><input type="email" className="text-field-6 w-input" maxLength={256} name="email" data-name="email" placeholder="Enter your email" id="email-3" required /><input type="submit" value="Talk To A Strategist" data-wait="Please wait..." className="submit-button-2 w-button" /></form>
+                <form id="email-form" name="email-form" data-name="Email Form" method="post" action="/api/submit" className="form-4"><input type="email" className="text-field-6 w-input" maxLength={256} name="email" data-name="email" placeholder="Enter your email" id="email-3" required /><input type="submit" value="Talk To A Strategist" data-wait="Please wait..." className="submit-button-2 w-button" /></form>
                 <div className="w-form-done">
                   <div>Thank you! Your submission has been received!</div>
                 </div>
