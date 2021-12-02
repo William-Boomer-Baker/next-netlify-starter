cd6c0b0306a62b1deed85e5994c4da12074c9c86
William-Boomer-Baker <61856702+William-Boomer-Baker@users.noreply.github.com>
2021-12-02 12:34:18 -0800

    Update appointment.js

--- pages/appointment.js
+++ pages/appointment.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { InlineWidget } from 'react-calendly'
 
 export default function Appointment() {
   return (
