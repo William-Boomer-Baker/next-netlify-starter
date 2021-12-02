b9ddf8550b50ef87aa55fa2974e6aae2e1402815
William-Boomer-Baker <61856702+William-Boomer-Baker@users.noreply.github.com>
2021-12-02 12:36:12 -0800

    Update appointment.js

--- pages/appointment.js
+++ pages/appointment.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import { InlineWidget } from 'react-calendly'
 
 export default function Appointment() {
   return (
@@ -9,17 +8,6 @@ export default function Appointment() {
           <div className="div-block-137">
             <h1 className="heading-55">Get In Touch With Us!</h1>
             <p className="paragraph-37">Want to go ahead and schedule an appointment? Use the calendar below to find the best time that works for you.</p>
-          </div>
-          <div className="html-embed w-embed">
-            {/*  Calendly inline widget begin  */}
-            <InlineWidget
-              url="https://calendly.com/hyperscalemedia/30min"
-              styles={{
-                minWidth: '320px',
-                height: '675px'
-              }}
-            />
-            {/*  Calendly inline widget end  */}
           </div>
         </div>
   )
